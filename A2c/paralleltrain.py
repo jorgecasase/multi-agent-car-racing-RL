c11530c0a2630089200409420ca19a34581f5754
Jorge Casas <jorgecasase@gmail.com>
2024-12-17 10:48:28 +0100

    update

--- A2c/paralleltrain.py
+++ A2c/paralleltrain.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     os.makedirs(log_dir, exist_ok=True)
 
     # Número de entornos paralelos
-    num_envs = 10  # Ajusta según los recursos de tu sistema
+    num_envs = 100  # Ajusta según los recursos de tu sistema
 
     # Crear múltiples entornos vectorizados con VecMonitor
     train_env = make_multi_env(num_envs)
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     )
 
     # Entrenar el modelo
-    max_episodes = 10000  # Cambia según tus necesidades
+    max_episodes = 5000  # Cambia según tus necesidades
     callback = StopTrainingOnEpisodes(max_episodes=max_episodes, verbose=1)
     model.learn(total_timesteps=int(1e6), callback=callback)
     model.save("a2c_multi_car_racing")
